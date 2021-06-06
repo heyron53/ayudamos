99172b4aa3b5abfa08ff793256dd035a44b9eca3
Aarón <heyronbd@gmail.com>
2021-06-06 21:58:53 +0200

    Logotipo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
             <div class="container">
                 <a class="navbar-brand text-light" href="{{ url('/') }}">
-                    AyudaMos
+                    <img src="{{ asset('img/iconos/logo.png') }}" weight="50px" height="50px">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
